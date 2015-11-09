48a22fee016f15af5dfd9b4e3c3d2ebcd6405169
Scott Pakin <pakin@lanl.gov>
2015-11-09 14:05:24 -0700

    Complete example of solver usage

--- clp.go
+++ clp.go
@@ -17,7 +17,8 @@ Linear programming is an optimization technique.  Given an objective function,
 such as a + b, and a set of constraints in the form of linear inequalities,
 such as 0 ≤ 2a + b ≤ 10 and 3 ≤ 2b − a ≤ 8, find values for the variables that
 maximizes or minimizes the objective function.  In this example, the maximum
-value of a + b is 7.6, which is achieved when a = 2.4 and b = 5.2.
+value of a + b is 7.6, which is achieved when a = 2.4 and b = 5.2.  (A package
+example below shows how to set up and solve this precise problem.)
 
 The Go package currently implements only a tiny subset of the CLP library
 but a subset that suffices to solve basic optimization problems.
--- simplex_test.go
+++ simplex_test.go
@@ -4,6 +4,7 @@
 package clp_test
 
 import (
+	"fmt"
 	"github.com/losalamos/clp"
 	"math"
 	"testing"
@@ -28,8 +29,43 @@ func closeTo(a, b, tol float64) bool {
 
 // Test if we can solve a complete optimization problem with the simplex model.
 func TestPrimalSolve(t *testing.T) {
-	// Set up the following problem: Maximize a + b subject to {0 ≤ 2a + b
-	// ≤ 10, 3 ≤ 2b − a ≤ 8}.
+	// Set up the following problem: Minimize a + 2b subject to {4 ≤ a + b
+	// ≤ 9, -5 ≤ 3a − b ≤ 3}.
+	mat := clp.NewPackedMatrix()
+	mat.AppendColumn([]clp.Nonzero{
+		{Index: 0, Value: 1.0}, // a
+		{Index: 1, Value: 3.0}, // 3a
+	})
+	mat.AppendColumn([]clp.Nonzero{
+		{Index: 0, Value: 1.0},  // b
+		{Index: 1, Value: -1.0}, // -b
+	})
+	rb := []clp.Bounds{
+		{Lower: 4, Upper: 9},  // [4, 9]
+		{Lower: -5, Upper: 3}, // [-5, 3]
+	}
+	obj := []float64{1.0, 2.0} // a + 2b
+	simp := clp.NewSimplex()
+	simp.LoadProblem(mat, nil, obj, rb, nil)
+	simp.SetOptimizationDirection(clp.Minimize)
+
+	// Solve the optimization problem.
+	simp.Primal(clp.NoValuesPass, clp.NoStartFinishOptions)
+	v := simp.ObjectiveValue()
+	soln := simp.PrimalColumnSolution()
+
+	// Check the results.
+	if !closeTo(soln[0], 1.75, 0.005) || !closeTo(soln[1], 2.25, 0.005) {
+		t.Fatalf("Expected [1.75 2.25] but observed %v", soln)
+	}
+	if !closeTo(v, 6.25, 0.005) {
+		t.Fatalf("Expected 6.25 but observed %.10g", v)
+	}
+}
+
+// Maximize a + b subject to both 0 ≤ 2a + b ≤ 10 and 3 ≤ 2b − a ≤ 8.
+func Example_complete() {
+	// Set up the problem.
 	mat := clp.NewPackedMatrix()
 	mat.AppendColumn([]clp.Nonzero{
 		{Index: 0, Value: 2.0},  // 2a
@@ -50,14 +86,13 @@ func TestPrimalSolve(t *testing.T) {
 
 	// Solve the optimization problem.
 	simp.Primal(clp.NoValuesPass, clp.NoStartFinishOptions)
-	v := simp.ObjectiveValue()
+	val := simp.ObjectiveValue()
 	soln := simp.PrimalColumnSolution()
 
-	// Check the results.
-	if !closeTo(v, 7.6, 0.05) {
-		t.Fatalf("Expected 7.6 but observed %.10g", v)
-	}
-	if !closeTo(soln[0], 2.4, 0.05) || !closeTo(soln[1], 5.2, 0.05) {
-		t.Fatalf("Expected [2.4 5.2] but observed %v", soln)
-	}
+	// Output the results.
+	fmt.Printf("a = %.1f\nb = %.1f\na + b = %.1f\n", soln[0], soln[1], val)
+	// Output:
+	// a = 2.4
+	// b = 5.2
+	// a + b = 7.6
 }
